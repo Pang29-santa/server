b931f16820fd5849e0a9bd698e4f3feb65cbad33
Pang29-santa <651413010@crru.ac.th>
2023-04-27 17:01:50 +0700

    Api

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import {
+  Avatar,
+  Button,
+  TextField,
+  Box,
+  Typography,
+  Grid,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Formik, Form, Field, FormikHelpers } from "formik";
+import { object, string } from "yup";
+import { signIn } from "@/services/serverService";
+import { useNavigate } from "react-router-dom";
+
+const SignIn = () => {
+  const navigate = useNavigate()  
+
+  interface loginForm {
+    username: string
+    password:string
+  }
+
+  const initialValues: loginForm = {
+    username: "",
+    password: ""
+  }
+
+  const formValidation = object({
+    username: string().required("Please enter email").email("Invalid email"),
+    password: string()
+      .required("Please enter password")
+      .min(5, "Password shold be minimum 6 charaters"),
+  });
+
+  const handleSubmitForm = (
+    values: loginForm,
+    formikHelper: FormikHelpers<loginForm>
+  ) => {
+    formikHelper.resetForm()
+    signIn(values)
+      .then((response) => {
+        const { success } = response
+        if (success) navigate('/dashboard', { replace: true })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  return (
+    <Box className="mt-8 mx-8 flex flex-col items-center">
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={formValidation}
+        onSubmit={handleSubmitForm}
+      >
+        {({ errors, isValid, touched, dirty }) => (
+          <Form>
+            <Box sx={{ mt: 1 }}>
+              <Field
+                margin="normal"
+                required
+                fullWidth
+                as={TextField}
+                id="username"
+                label="Email Address"
+                name="username"
+                autoComplete="email"
+                autoFocus
+                error={Boolean(errors.username) && Boolean(touched.username)}
+                helperText={Boolean(touched.username) && errors.username}
+              />
+              <Field
+                margin="normal"
+                required
+                fullWidth
+                as={TextField}
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={Boolean(errors.password) && Boolean(touched.password)}
+                helperText={Boolean(touched.password) && errors.password}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={!dirty || !isValid}
+              >
+                Sign In
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+export default SignIn;
---
+++ src/services/serverService.ts
@@ -0,0 +1,17 @@
+import { SignIn } from "@/models/signin.model"
+import httpClient from "@/utils/httpClient"
+
+
+type SignInProps =  {
+    username: string
+    password: string
+}
+
+export const signIn = async (credential: SignInProps) : Promise<SignIn> => {
+    const {data: response} = await httpClient.post<SignIn>('/auth' , credential)
+    return response
+}
+
+export const signOut = async () : Promise<void> => {
+    await httpClient.post('auth/logout,null')
+}
